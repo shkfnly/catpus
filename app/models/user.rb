6cb9fa00bf164af57c8decf2e1ca80ba258a31ee
shkfnly <ashoka.finley@gmail.com>
2015-02-03 11:50:22 -0800

    created User Show

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # validates :username, :email, :password_digest, :session_token, presence: true
   validates :name, :email, uniqueness: true
-  validates :password, length: {minimum: 6, allow_nil: true}
+
 
   attr_reader :password
 
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do 
-  provider :github, Rails.application.secrets['GITHUB_KEY'], Rails.application.secrets['GITHUB_SECRET'], scope: "user,public_repo,repo,repo:status,notifications"
+  provider :github, Rails.application.secrets['GITHUB_KEY'], Rails.application.secrets['GITHUB_SECRET'], scope: "user,public_repo,repo,repo:status,read:org,admin:repo_hook"
 end
