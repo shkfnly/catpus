690662d0a6566ed48686cc0459d1ae36b27d6f85
shkfnly <ashoka.finley@gmail.com>
2015-02-05 00:08:38 -0800

    Also revert

--- db/migrate/20150205073520_fix_schema.rb
+++ db/migrate/20150205073520_fix_schema.rb
@@ -1,6 +1,5 @@
 class FixSchema < ActiveRecord::Migration
   def change
-    rename_column :card_assignments, :issue_id, :card_id
     remove_column :lists, :repo_id, :integer
     add_column :lists, :board_id, :integer
   end
