419580dc9b207bb40180bce75f39da18d20375c4
shkfnly <ashoka.finley@gmail.com>
2015-02-06 17:12:18 -0800

    I might be dumb

--- app/controllers/api/hooks_controller.rb
+++ app/controllers/api/hooks_controller.rb
@@ -5,15 +5,13 @@ class Api::HooksController < Api::ApiController
   def create
     board = Board.find(params[:board_id])
     unless params[:zen]
+      card = board.cards.find_by(title: params[:head_commit][:message])
+      if card
+        card.remove
+      end
       board.update(pushed_at: params[:head_commit][:timestamp])
       repo = Repository.find_by(github_id: board.repository_id)
       repo.update(pushed_at: params[:head_commit][:timestamp])
-    end
-    if params[:commits]
-      card = board.cards.find_by(title: params[:commits].first[:message])
-      if card
-        card.destroy
-      end
     end
     Pusher['boards'].trigger('webhook-push', {board: board}, request.headers["X-Pusher-Socket-ID"])
     render json: board
